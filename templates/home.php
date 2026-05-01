3fc07a48b9418dbbda823b97744e957062bfc82f
Davy Jean-Charles <dave.jc971@gmail.com>
2026-05-01 18:48:57 -0400

    Starting the PHP App, with new Template Home, with the body of the folder created.

---
+++ templates/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <h1>Touche pas au klaxon</h1>
+        <div>
+            <p>Connexion</p>
+        </div>
+    </header>
+    <main>
+        <table>
+            <caption>Pour obtenir plus d'informations sur un trajet, veuillez vous connecter</caption>
+            <thead>
+                <tr>
+                    <th>Départ</th>
+                    <th>Date</th>
+                    <th>Destination</th>
+                    <th>Date</th>
+                    <th>Places</th>
+                </tr>
+            </thead>
+        </table>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
